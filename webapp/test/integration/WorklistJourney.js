1355d2ea551940f9a7b432c0bcbcb58fe35fccbd
ironyhouse <36641168+ironyhouse@users.noreply.github.com>
2020-12-30 19:04:13 +0300

    feat: add OPA tests

--- webapp/test/integration/WorklistJourney.js
+++ webapp/test/integration/WorklistJourney.js
@@ -1,23 +1,29 @@
-/*global QUnit*/
+sap.ui.define(["sap/ui/test/opaQunit", "./pages/Worklist"], function (opaTest) {
+    "use strict";
 
-sap.ui.define([
-	"sap/ui/test/opaQunit",
-	"./pages/Worklist"
-], function (opaTest) {
-	"use strict";
+    QUnit.module("Posts");
 
-	QUnit.module("Posts");
+    opaTest(
+        "Should see the table with all posts",
+        function (Given, When, Then) {
+            // Arrangements
+            Given.iStartMyApp();
 
-	opaTest("Should see the table with all posts", function (Given, When, Then) {
-		// Arrangements
-		Given.iStartMyApp();
+            // Assertions
+            Then.onTheWorklistPage
+                .theTableShouldHavePagination()
+                // .and.theTitleShouldDisplayTheTotalAmountOfItems();
+        }
+    );
 
-		// Assertions
-		Then.onTheWorklistPage.theTableShouldHaveAllEntries().
-			and.theTitleShouldDisplayTheTotalAmountOfItems();
+    // opaTest("Should be able to load more items", function (Given, When, Then) {
+    // 	//Actions
+    // 	When.onTheWorklistPage.iPressOnMoreData();
 
-		// Cleanup
-		Then.iTeardownMyApp();
-	});
+    // 	// Assertions
+    // 	Then.onTheWorklistPage.theTableShouldHaveAllEntries();
 
+    // 	// Cleanup
+    // 	Then.iTeardownMyApp();
+    // });
 });
--- webapp/test/integration/pages/Worklist.js
+++ webapp/test/integration/pages/Worklist.js
@@ -1,50 +1,77 @@
 sap.ui.define([
 		'sap/ui/test/Opa5',
 		'sap/ui/test/matchers/AggregationLengthEquals',
-		'sap/ui/test/matchers/I18NText'
+		'sap/ui/test/matchers/I18NText',
+		'sap/ui/test/actions/Press'
 	],
 	function (Opa5,
 			  AggregationLengthEquals,
-			  I18NText) {
+			  I18NText,
+			  Press) {
 		"use strict";
 
-		var sViewName = "Worklist",
-			sTableId = "table";
+		var sViewName = "Categories",
+			sTableId = "CategoriesTable";
 
 		Opa5.createPageObjects({
 			onTheWorklistPage: {
-				actions: {},
+				// actions: {
+				// 	iPressOnMoreData: function () {
+				// 		// Press action hits the "more" trigger on a table
+				// 		return this.waitFor({
+				// 			id: sTableId,
+				// 			viewName: sViewName,
+				// 			actions: new Press(),
+				// 			errorMessage: "The table does not have a trigger"
+				// 		});
+				// 	}
+				// },
 				assertions: {
-					theTableShouldHaveAllEntries: function () {
+					theTableShouldHavePagination: function () {
 						return this.waitFor({
 							id: sTableId,
 							viewName: sViewName,
 							matchers: new AggregationLengthEquals({
 								name: "items",
-								length: 23
+								length: 4
 							}),
 							success: function () {
-								Opa5.assert.ok(true, "The table has 23 items");
+								Opa5.assert.ok(true, "The table has items on the first page");
 							},
 							errorMessage: "The table does not contain all items."
 						});
 					},
 
-					theTitleShouldDisplayTheTotalAmountOfItems: function () {
-						return this.waitFor({
-							id: "tableHeader",
-							viewName: sViewName,
-							matchers: new I18NText({
-								key: "worklistTableTitleCount",
-								propertyName: "text",
-								parameters: [23]
-							}),
-							success: function () {
-								Opa5.assert.ok(true, "The table header has 23 items");
-							},
-							errorMessage: "The table header does not contain the number of items: 23"
-						});
-					}
+					// theTableShouldHaveAllEntries: function () {
+					// 	return this.waitFor({
+					// 		id: sTableId,
+					// 		viewName: sViewName,
+					// 		matchers: new AggregationLengthEquals({
+					// 			name: "items",
+					// 			length: 23
+					// 		}),
+					// 		success: function () {
+					// 			Opa5.assert.ok(true, "The table has 23 items");
+					// 		},
+					// 		errorMessage: "The table does not contain all items."
+					// 	});
+					// },
+
+					// theTitleShouldDisplayTheTotalAmountOfItems: function () {
+					// 	return this.waitFor({
+					// 		id: "tableHeader",
+					// 		viewName: sViewName,
+					// 		matchers: new I18NText({
+					// 			key: "worklistTableTitleCount",
+					// 			propertyName: "text",
+					// 			parameters: [23]
+					// 		}),
+					// 		success: function () {
+					// 			Opa5.assert.ok(true, "The table header has 23 items");
+					// 		},
+					// 		errorMessage: "The table header does not contain the number of items: 23"
+					// 	});
+					// }
 
 				}
 			}
